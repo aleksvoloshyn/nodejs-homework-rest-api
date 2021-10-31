c1c5f1fab0b765f79f8daa2d701fba42fc9c3996
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2021-10-31 22:19:52 +0200

    router.get added

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,8 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const { NotFound, BadRequest } = require("http-errors");
+const Joi = require("joi");
+const contactsOperations = require("../../models/contacts");
+
+const joiSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+  phone: Joi.string()
+    .regex(/^\d{3}-\d{3}-\d{4}$/)
+    .required(),
+});
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "template message!" });
+  try {
+    const result = await contactsOperations.getAll();
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        result,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get("/:contactId", async (req, res, next) => {
