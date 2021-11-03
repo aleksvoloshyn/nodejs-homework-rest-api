eba48bbab382fc17785213b4470dc71135b3a698
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2021-11-03 17:03:59 +0200

    router fixed

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -5,7 +5,7 @@ const Joi = require("joi");
 const contactsOperations = require("../../models/contacts/index");
 
 const joiSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
+  name: Joi.string().min(1).max(30).required(),
   email: Joi.string().email({ minDomainSegments: 2 }).required(),
   phone: Joi.string()
     .regex(/^\d{3}-\d{3}-\d{4}$/)
@@ -31,9 +31,6 @@ router.get("/:id", async (req, res, next) => {
   try {
     const { id } = req.params;
     const result = await contactsOperations.getById(id);
-    if (!result) {
-      throw new NotFound(`Contact with id=${id} not found`);
-    }
     res.json({
       status: "success",
       code: 200,
