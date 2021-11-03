c7a96c5124f5b5b89d50797e56a0644619154e68
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2021-11-03 16:58:00 +0200

    contact table removed

--- models/contacts/updateById.js
+++ models/contacts/updateById.js
@@ -1,16 +1,15 @@
-const getAll = require('./getAll')
-const updateContacts = require('./updateContacts')
+const getAll = require("./getAll");
+const updateContacts = require("./updateContacts");
 
 const updateById = async (id, name, email, phone) => {
-  const contacts = await getAll()
-  const index = contacts.findIndex((item) => item.id === id)
+  const contacts = await getAll();
+  const index = contacts.findIndex((item) => item.id === id);
   if (index === -1) {
-    return null
+    return null;
   }
-  contacts[index] = { id, name, email, phone }
-  await updateContacts(contacts)
-  console.table(await getAll())
-  return contacts[index]
-}
+  contacts[index] = { id, name, email, phone };
+  await updateContacts(contacts);
+  return contacts[index];
+};
 
-module.exports = updateById
+module.exports = updateById;
