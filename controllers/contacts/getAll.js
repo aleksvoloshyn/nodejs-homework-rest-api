9aa2bfaa237baae1ce8240f620d6e9362678608f
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2021-11-04 18:02:36 +0200

    lint fixed

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,14 +1,14 @@
-const contactsOperations = require("../../models/contacts");
+const contactsOperations = require('../../models/contacts')
 
 const getAll = async (req, res) => {
-  const result = await contactsOperations.getAll();
+  const result = await contactsOperations.getAll()
   res.json({
-    status: "success",
+    status: 'success',
     code: 200,
     data: {
       result,
     },
-  });
-};
+  })
+}
 
-module.exports = getAll;
+module.exports = getAll
