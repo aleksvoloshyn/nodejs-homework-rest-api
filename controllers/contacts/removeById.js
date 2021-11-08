3836f88753fc7582d0ae3068411a9fc035b411b3
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2021-11-08 14:41:53 +0200

    removeContact added

--- controllers/contacts/removeById.js
+++ controllers/contacts/removeById.js
@@ -1,13 +1,18 @@
-const contactsOperations = require('../../models/contacts')
+// const contactsOperations = require('../../models/contacts')
+const { Contact } = require("../../models");
 
 const removeById = async (req, res) => {
-  const { id } = req.params
+  const { id } = req.params;
+  const result = await Contact.findByIdAndRemove(id);
+  if (!result) {
+    return next(new NotFound(`Contact with id=${contactId} not found`));
+  }
 
   res.json({
-    status: 'success',
+    status: "success",
     code: 200,
-    message: 'Remove success',
-  })
-}
+    message: "Remove success",
+  });
+};
 
-module.exports = removeById
+module.exports = removeById;
