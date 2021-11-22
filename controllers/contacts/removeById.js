1121b5141e35e7fbb822e914b682523174f2d3df
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2021-11-23 00:35:38 +0200

    owner removebyid

--- controllers/contacts/removeById.js
+++ controllers/contacts/removeById.js
@@ -16,4 +16,23 @@ const removeById = async (req, res) => {
   })
 }
 
+// const removeById = async (req, res) => {
+//   const { contactId } = req.params
+//   const { _id } = req.user
+//   console.log(req.user)
+//   const result = await Contact.findOneAndRemove({
+//     owner: _id,
+//     _id: contactId,
+//   })
+//   if (!result) {
+//     throw new NotFound(`Contact with id=${contactId} not found`)
+//   }
+
+//   res.json({
+//     status: 'success',
+//     code: 200,
+//     message: 'Remove success',
+//   })
+// }
+
 module.exports = removeById
