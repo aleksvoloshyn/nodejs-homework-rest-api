5958fd8d339cb13f282748ae6a3cedae0ed25a5e
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2021-11-22 12:58:36 +0200

    isNaN check added

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,9 +1,16 @@
 // const contactsOperations = require("../../models/contacts");
 const { Contact } = require('../../models')
+const { BadRequest } = require('http-errors')
 
-const getAll = async (req, res) => {
+const getAll = async (req, res, next) => {
   const { page, limit } = req.query
+
+  if (isNaN(page) || isNaN(limit)) {
+    return next(new BadRequest())
+  }
+
   const { _id } = req.user
+
   const skip = (page - 1) * limit
   const result = await Contact.find(
     { owner: _id },
