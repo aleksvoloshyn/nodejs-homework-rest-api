1278d84f3f10b7f8a5f0fab67309fe589fd4cf36
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2021-11-23 11:17:21 +0200

    getById owner added

--- controllers/contacts/getById.js
+++ controllers/contacts/getById.js
@@ -1,10 +1,21 @@
 // const { contactsOperations } = require('../../models/contacts')
 const { Contact } = require('../../models')
+const { NotFound } = require('http-errors')
 
-const getById = async (req, res) => {
+const getById = async (req, res, next) => {
   const { id } = req.params
-  // const result = await Contact.findOne({ _id: id });
-  const result = await Contact.findById(id)
+  const { _id } = req.user
+  // const result = await Contact.findById(id)
+
+  const result = await Contact.findOne({ owner: _id, _id: id }).populate(
+    'owner',
+    '_id email'
+  )
+
+  if (!result) {
+    return next(new NotFound(`Contact with id=${id} not found`))
+  }
+
   res.json({
     status: 'success',
     code: 200,
